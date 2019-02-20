521878cf2dab84bc6279e49b054c5db697215c3c
AurelienAubry <aurelien.aubry.dev@gmail.com>
2019-02-20 22:26:16 +0100

    Added iproute2 bridges

--- netsim.py
+++ netsim.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import docker
+import pyroute2
 import yaml
 from termcolor import colored, cprint
 
@@ -10,30 +11,59 @@ client = docker.from_env()
 
 hosts = []
 host_names = []
+bridges = []
+links = []
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("cmd", help="command to run : start, stop")
 parser.add_argument("path", help="network file location")
 args = parser.parse_args()
+ipr = pyroute2.IPRoute()
 
 # Parse the network's yaml file and get hosts / host names
 def parse_yaml(path):
 	with open(path, 'r') as stream:
-		topology = yaml.load(stream)
+		network = yaml.load(stream)
 		
 		global hosts
 		global host_names
+		global bridges
+		global links
+
+		topology = network['network']
 		hosts = topology['hosts']
 		host_names = [ h['name'] for h in hosts ]
+		bridges = topology['bridges']
+		links = topology['links']
 
 
 # Run the network (run containers)
 def start_network():
+	start_bridges()
+	start_hosts()
+
+def start_bridges():
+	cprint('======[ BRIDGES ]======', 'blue', end='\n')
+	for bridge in bridges:
+		bridge_name = bridge['name']
+		bridge_address = bridge['address']
+
+		bridge_address_mask = bridge_address.split('/')
+
+		ipr.link("add", ifname=bridge_name, kind="bridge")
+		dev = ipr.link_lookup(ifname=bridge_name)[0]
+
+		ipr.link("set", index=dev, state="up")
+		ipr.addr("add", index=dev, address=bridge_address_mask[0],mask=int(bridge_address_mask[1]))
+
+		cprint(bridge_name + ' added', 'green', end='\n')
+
+def start_hosts():
+	cprint('======[ HOSTS ]======', 'blue', end='\n')
 	for host in hosts:
 		container_image = host['image']
 		container_name = host['name']
-		
 		# Container with volume
 		if 'volume' in host: 
 			volume = host['volume']
@@ -49,10 +79,24 @@ def start_network():
 
 # Stop the network (stop and remove containers)
 def stop_network():
+	stop_hosts()
+	stop_bridges()
+	
+def stop_hosts():
+	cprint('======[ HOSTS ]======', 'blue', end='\n')
 	for container_name in host_names:
 		client.containers.get(container_name).stop(timeout=0)
 		client.containers.get(container_name).remove()
 		cprint(container_name + ' stopped', 'green', end='\n')
+
+def stop_bridges():
+	cprint('======[ BRIDGES ]======', 'blue', end='\n')
+	for bridge in bridges:
+		bridge_name = bridge['name']
+		dev = ipr.link_lookup(ifname=bridge_name)[0]
+		ipr.link("delete", index=dev, kind="bridge")
+		cprint(bridge_name + ' removed', 'green', end='\n')
+		
 
 
 parse_yaml(args.path)
